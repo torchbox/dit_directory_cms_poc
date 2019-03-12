adcf32e7f9a05da16abb863194fd248d43998701
Thibaud Colas <thibaudcolas@gmail.com>
2019-03-12 19:08:30 +0000

    Add WIP link chooser integration for Markdown editors

--- richtext_poc/static/richtext_poc/js/refresh_codemirror.js
+++ richtext_poc/static/richtext_poc/js/refresh_codemirror.js
@@ -1,13 +1,84 @@
-function simplemdeAttach(id) {
-  var mde = new SimpleMDE({
-    element: document.getElementById(id),
-    autofocus: false
-  });
-  mde.render();
+function _replaceSelection(cm, active, startEnd, url) {
+    if (
+        /editor-preview-active/.test(cm.getWrapperElement().lastChild.className)
+    )
+        return;
 
-  mde.codemirror.on("change", function() {
-    $("#" + id).val(mde.value());
-  });
+    var text;
+    var start = startEnd[0];
+    var end = startEnd[1];
+    var startPoint = cm.getCursor('start');
+    var endPoint = cm.getCursor('end');
+    if (url) {
+        end = end.replace('#url#', url);
+    }
+    if (active) {
+        text = cm.getLine(startPoint.line);
+        start = text.slice(0, startPoint.ch);
+        end = text.slice(startPoint.ch);
+        cm.replaceRange(start + end, {
+            line: startPoint.line,
+            ch: 0,
+        });
+    } else {
+        text = cm.getSelection();
+        cm.replaceSelection(start + text + end);
+
+        startPoint.ch += start.length;
+        if (startPoint !== endPoint) {
+            endPoint.ch += start.length;
+        }
+    }
+    cm.setSelection(startPoint, endPoint);
+    cm.focus();
+}
+
+const realPrompt = window.prompt;
+function openLinkChooser(mde, text) {
+    if (text === 'URL for the link:') {
+        const workflow = window.ModalWorkflow({
+            url: window.chooserUrls.pageChooser,
+            urlParams: {
+                page_type: 'wagtailcore.page',
+                allow_external_link: true,
+                allow_email_link: true,
+                link_text: '',
+            },
+            onload: window.PAGE_CHOOSER_MODAL_ONLOAD_HANDLERS,
+            responses: {
+                pageChosen: (data) => {
+                    console.log(data);
+                    _replaceSelection(
+                        mde.codemirror,
+                        mde.getState().link,
+                        ['[', '](#url#)'],
+                        `slug:${data.id || data.url}`,
+                    );
+                    workflow.close();
+                },
+            },
+            onError: () => {
+                window.alert('Error');
+            },
+        });
+        return '';
+    }
+    return realPrompt(text);
+}
+
+function simplemdeAttach(id) {
+    var mde = new SimpleMDE({
+        element: document.getElementById(id),
+        promptURLs: true,
+        autofocus: false,
+    });
+    mde.render();
+
+    mde.codemirror.on('change', function() {
+        $('#' + id).val(mde.value());
+    });
+
+    window.prompt = openLinkChooser.bind(null, mde);
 }
 
 // Refresh the markdown entry field when the tab buttons are clicked.
@@ -15,20 +86,20 @@ function simplemdeAttach(id) {
 // if it is hidden. Tab contents are of course hidden before they are clicked.
 // Unless this is done the markdown entry field will appear empty until clicked.
 (function() {
-  var checkExist = setInterval(function() {
-    if (document.getElementsByClassName("tab-nav")) {
-      clearInterval(checkExist);
-      $(".tab-nav").on("shown.bs.tab", refreshMarkdownFields);
-    }
-  }, 100);
+    var checkExist = setInterval(function() {
+        if (document.getElementsByClassName('tab-nav')) {
+            clearInterval(checkExist);
+            $('.tab-nav').on('shown.bs.tab', refreshMarkdownFields);
+        }
+    }, 100);
 
-  function refreshMarkdownFields(event) {
-    var id = event.target.getAttribute("href").replace("#", "");
-    var tabContent = document.getElementById(id);
-    var codeElements = tabContent.getElementsByClassName("CodeMirror");
-    for (var j = 0; j < codeElements.length; j++) {
-      var codeElement = codeElements[j];
-      codeElement.CodeMirror.refresh();
+    function refreshMarkdownFields(event) {
+        var id = event.target.getAttribute('href').replace('#', '');
+        var tabContent = document.getElementById(id);
+        var codeElements = tabContent.getElementsByClassName('CodeMirror');
+        for (var j = 0; j < codeElements.length; j++) {
+            var codeElement = codeElements[j];
+            codeElement.CodeMirror.refresh();
+        }
     }
-  }
 })();
