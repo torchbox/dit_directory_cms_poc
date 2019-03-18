0788fa47137d00ec41a297a61fb637ddd29cde05
Thibaud Colas <thibaudcolas@gmail.com>
2019-03-18 11:54:17 +0000

    Tentatively add client-side compilation + bundling with Webpack

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                modules: false,
+            },
+        ],
+        '@babel/preset-react',
+        '@babel/preset-flow',
+    ],
+    env: {
+        test: {
+            presets: [
+                '@babel/preset-env',
+                '@babel/preset-react',
+                '@babel/preset-flow',
+            ],
+        },
+    },
+};
---
+++ richtext_poc/static_src/richtext_poc.entry.js
@@ -0,0 +1,1 @@
+console.log('richtext_poc');
